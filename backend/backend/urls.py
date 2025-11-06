220923ee5a2a668e372e4b8681825d71ef1645c7
kevin-murithi <murithikevin54@gmail.com>
2025-11-06 22:09:59 +0300

    chore: add gitignore/gitattributes

--- backend/backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from rest_framework import permissions
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('api/auth/', include('accounts.urls')),
-]
